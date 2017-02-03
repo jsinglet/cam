dd88b3fdd64a89db0570afe414b7ed0b42f43a96
John L. Singleton <jsinglet@gmail.com>
2017-02-03 17:01:04 -0500

    added scotty as a dependency

--- cam-sim/Main.hs
+++ cam-sim/Main.hs
@@ -1,7 +1,15 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+
 module Main where
 
 import Lexer
+import Web.Scotty
 
+import Data.Monoid (mconcat)
 
-main :: IO ()
-main = putStrLn $  show $ getTokens "hello world"
+main = scotty 3000 $ do
+  get "/:word" $ do
+    beam <- param "word"
+    html $ mconcat ["<h1>Scotty, ", beam, " me up!</h1>"]
+
